5143bc874b87a6242a432fffb72b61a7c8e1d18c
Maheen Begum <maheen.begum0173@gmail.com>
2026-09-17 23:16:26 +0530

    Fix database connection

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,18 @@
 import mysql.connector
-from mysql.connector import pooling
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-connection_pool = pooling.MySQLConnectionPool(
-    pool_name="studify_pool",
-    pool_size=5,
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
 
 def get_connection():
-    return connection_pool.get_connection()
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        connection_timeout=10
+    )
+
+    return connection
